49d67281e2b43a4e089cd7d3c9991cbfe2daa71e
Daniely Marcon <140666056+daniely-marcon@users.noreply.github.com>
2026-05-07 17:04:38 -0400

    primeiro commit

---
+++ AcolhePet/src/main/java/com/mycompany/acolhepet/AcolhePet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.acolhepet;
+
+/**
+ *
+ * @author danie
+ */
+public class AcolhePet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
